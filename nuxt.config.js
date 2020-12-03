ced86172293970d864e6dee10ee83237d8c05b89
jiancheng_wang <jiancheng_wang@yahoo.com>
2020-12-03 18:33:01 +0800

    test github action 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   head: {
     title: 'my-site',
     meta: [
-      { charset: 'utf-8' },
+      { lang: 'zh', charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
