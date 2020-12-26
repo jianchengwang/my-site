e9b2549226754915d10ef03783b9e127e6c9ff25
jiancheng_wang <jiancheng_wang@yahoo.com>
2020-12-26 20:43:24 +0800

    test shelljs 6

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const app = require('express')();
 const shell = require('shelljs');
 
-app.get('/hello', (req, res) => {
-  res.send(shell.pwd())
-})
-
 app.get('/downgit', (req, res) => {
   let query = req.query
   const gitrep = query.gitrep
-  const downloadLink = shell.exec(`tail -n 1 $(/root/_sh/downgit/downgit.sh '${gitrep}')`)
+  const downloadLink = shell.exec(`tail -n 2 $(/root/_sh/downgit/downgit.sh '${gitrep}')`)
   res.send(downloadLink)
 })
 
