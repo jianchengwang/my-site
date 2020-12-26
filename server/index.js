c9c203d1e0ccd54b240259943e594dedebc7b998
jiancheng_wang <jiancheng_wang@yahoo.com>
2020-12-26 21:07:37 +0800

    test shelljs 9

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const shell = require('shelljs');
 app.get('/downgit', (req, res) => {
   let query = req.query
   const gitrep = query.gitrep
-  const downloadLink = shell.exec("${$(/root/_sh / downgit / downgit.sh '" + gitrep + "')#downloadLink:}")
+  targetFilePath = "${gitrep#*${branch}/}"
+  const downloadLink = shell.exec(`tail -n 2 $(/root/_sh/downgit/downgit.sh '${gitrep}')#*downloadLink:`)
   res.send(downloadLink)
 })
 
